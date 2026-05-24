fd8c8d5a596f60fcd000687115901f5885c4d58f
Turgay Karakus <barut525252@gmail.com>
2026-05-24 09:24:18 +0200

    add pwa support

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,45 @@
 /** @type {import('next').NextConfig} */
+
+import withPWAInit
+from "next-pwa";
+
+const withPWA =
+  withPWAInit({
+
+    dest: "public",
+
+    register: true,
+
+    skipWaiting: true,
+
+    disable:
+      process.env.NODE_ENV ===
+      "development",
+  });
+
 const nextConfig = {
+
   images: {
+
     remotePatterns: [
+
       {
         protocol: "https",
-        hostname: "pobizaxvmxgqfxwmhvq.supabase.co",
+
+        hostname:
+          "pobizaxvmxgqfxwmhvq.supabase.co",
+      },
+
+      {
+        protocol: "https",
+
+        hostname:
+          "**",
       },
     ],
   },
 };
 
-export default nextConfig;
+export default withPWA(
+  nextConfig
+);

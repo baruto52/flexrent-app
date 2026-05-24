2c6508a5b6842a7e9054be7369d514181a023cb4
Turgay Karakus <barut525252@gmail.com>
2026-05-24 10:34:55 +0200

    setup push notifications

---
+++ lib/push.ts
@@ -0,0 +1,15 @@
+import webpush
+from "web-push";
+
+webpush.setVapidDetails(
+
+  "mailto:support@flexrent.de",
+
+  process.env
+    .NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
+
+  process.env
+    .VAPID_PRIVATE_KEY!
+);
+
+export default webpush;
---
+++ public/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener(
+  "push",
+  (event) => {
+
+    const data =
+      event.data.json();
+
+    self.registration.showNotification(
+
+      data.title,
+
+      {
+        body:
+          data.body,
+
+        icon:
+          "/icon-192.png",
+      }
+    );
+  }
+);

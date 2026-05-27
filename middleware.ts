dfcf016c3a2f769ff71d74566e416be6c74dae0e
Turgay Karakus <barut525252@gmail.com>
2026-05-27 21:06:36 +0200

    fix manifest middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,32 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function middleware(
+  req: NextRequest
+) {
+
+  const pathname =
+    req.nextUrl.pathname;
+
+  // PWA FILES IMMER ERLAUBEN
+
+  if (
+    pathname === "/manifest.json" ||
+    pathname === "/sw.js" ||
+    pathname.startsWith("/icons") ||
+    pathname.startsWith("/icon-") ||
+    pathname.startsWith("/workbox-")
+  ) {
+
+    return NextResponse.next();
+  }
 
   return NextResponse.next();
-
 }
 
 export const config = {
+
   matcher: [
-    "/favorites/:path*",
-    "/messages/:path*",
-    "/notifications/:path*",
-    "/profile/:path*",
-    "/create/:path*",
-    "/bookings/:path*",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
